65f0051c9e07aed3645e48563e0da7f962e9375c
Halit Kaplan <1halitkaplan@gmail.com>
2021-09-13 23:11:37 +0300

    Arayüz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarComponent } from './components/car/car.component';
+import { BrandComponent } from './components/brand/brand.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { SearchComponent } from './components/search/search.component';
+import { CommentsComponent } from './components/comments/comments.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarComponent,
+    BrandComponent,    
+    NavbarComponent, CarouselComponent, ModalComponent, SearchComponent, CommentsComponent
   ],
   imports: [
     BrowserModule,
